6660438025597c056a609bf09a3062620e82abdd
Anthonyph <anthony.phim@gmail.com>
2019-10-14 13:30:13 +0200

    first commit

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,15 @@
-class User
+require "pry"
 
+class User
+	attr_accessor :email
+	attr_reader :age
+	@@user_count = 0
+
+	def initialize(email_to_save, age_to_save)
+	@email = email_to_save
+	@age = age_to_save
+	@@user_count = @@user_count + 1
+	end
 end
+	binding.pry 
+	put "fin du fichier"
